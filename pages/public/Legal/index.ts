7a498745324509c39f4d860a5fbc9f16380bfe26
Shahrial Amin <shahrial741@gmail.com>
2026-02-12 07:37:54 +0600

    Delete pages/public/Legal/index.ts

--- pages/public/Legal/index.ts
+++
@@ -1,5 +0,0 @@
-export { default } from './TermsPage';
-export { default } from './PrivacyPage';
-export { default } from './RefundPolicyPage';
-export { default } from './CookiePolicyPage';
-export { default } from './ShipmentPolicyPage';
